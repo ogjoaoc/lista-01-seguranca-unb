917b872e54270de5c939ea0a644bc2ff756520c7
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2025-04-21 01:18:22 -0300

    add transpo files (not final)

---
+++ cifra_transposicao.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// string removeEspacos(string &mensagem) {
+//     string retorno = "";
+//     for(char &letra : mensagem) {
+//         if(letra != ' ') {
+//             retorno += letra;
+//         }
+//     }
+//     return retorno;
+// }
+
+const map<string,double> TRIGRAFOS = {
+    {"que", 72.29}, {"ent", 70.23}, {"nte", 55.08}, {"ado", 51.16},
+    {"ade", 50.04}, {"ode", 45.43}, {"ara", 45.37}, {"est", 43.90},
+    {"res", 43.08}, {"con", 41.73}, 
+};
+
+const map<string,double> DIGRAFOS = {
+    {"de", 3.25}, {"es", 2.89}, {"en", 2.59}, {"nt", 2.44},
+    {"te", 2.38}, {"er", 2.28}, {"el", 2.05}, {"ra", 1.98}
+};
+
+double calculaScore(string &texto) {
+    map<string,int> contagem;
+    for(int i = 0; i < texto.length() - 1; i++) {
+        if(i < texto.length() - 2) {
+            string trigrafo = texto.substr(i, 3);
+            contagem[trigrafo]++;
+        }
+        string digrafo = texto.substr(i, 2);
+        contagem[digrafo]++;
+    }
+    double score = 0;
+    for(auto &[trigrafo, freq] : TRIGRAFOS) {
+        if(contagem[trigrafo]) {
+            score += contagem[trigrafo] * freq;
+        }
+    }
+    for(auto &[digrafo, freq] : DIGRAFOS) {
+        if(contagem[digrafo]) {
+            score += contagem[digrafo] * freq;
+        }
+    }
+    return score;
+}
+
+vector<pair<string,pair<double,vector<int>>>> ataqueFrequencia(string mensagem_criptografada, int TAM_CHAVE) {
+    vector<int> permutacao(TAM_CHAVE);
+    vector<pair<string,pair<double,vector<int>>>> possibilidades;
+    for(int i = 0; i < TAM_CHAVE; i++) {
+        permutacao[i] = i + 1;
+    }
+    do {
+        int colunas = TAM_CHAVE;
+        int linhas = mensagem_criptografada.length() / colunas;
+        vector<int> ordem_original(colunas);
+        for(int i = 0; i < colunas; i++) {
+            ordem_original[permutacao[i] - 1] = i;
+        }
+        string mensagem_decifrada;
+        for(int i = 0; i < linhas; i++) {
+            for(int &j : ordem_original) {
+                int pos = j  * linhas + i;
+                if(pos < mensagem_criptografada.size()) {
+                    mensagem_decifrada += mensagem_criptografada[pos];
+                }
+            }
+        }
+        double score = calculaScore(mensagem_decifrada);
+        possibilidades.push_back({mensagem_decifrada, {score, permutacao}});
+    } while(next_permutation(begin(permutacao), end(permutacao)));
+    sort(begin(possibilidades), end(possibilidades), [&](auto a, auto b) {
+       return a.second.first > b.second.first;
+    });
+    return possibilidades;
+}
+
+vector<pair<char,int>> geraPermutacao(string &chave) {
+    vector<pair<char,int>> contagem;
+    for(int i = 0; i < chave.size(); i++) {
+        contagem.push_back({chave[i], i + 1});
+    }
+    sort(contagem.begin(), contagem.end());
+    return contagem;
+}
+
+string codificaTransposicao(string mensagem, string chave) {
+    // mensagem = removeEspacos(mensagem);
+    int linhas = (mensagem.length() + chave.length() - 1) / chave.length(); 
+    int colunas = chave.size();
+    vector<vector<char>> matriz(linhas, vector<char> (colunas));
+    int indice = 0;
+    for(int i = 0; i < linhas; i++) {
+        for(int j = 0; j < colunas; j++) {
+            if(indice < mensagem.length()) {
+                matriz[i][j] = mensagem[indice++];
+            } else {
+                matriz[i][j] = 'X';
+            }
+        }
+    }
+    string mensagem_codificada = "";
+    vector<string> auxiliar;
+    for(int j = 0; j < colunas; j++) {
+        string aux = "";
+        for(int i = 0; i < linhas; i++) {
+            aux += matriz[i][j];
+        }
+        auxiliar.push_back(aux);
+    }
+    vector<pair<char, int>> permutacao = geraPermutacao(chave);
+    //debug for(auto &x : permutacao) cout << x.second << ' '; cout << '\n';
+    for(int i = 0; i < colunas; i++) {
+        mensagem_codificada += auxiliar[permutacao[i].second - 1];
+    }
+    return mensagem_codificada;
+}
+
+string decodificaTransposicao(string mensagem_criptografada, string chave) {
+    int colunas = chave.size();
+    int linhas = mensagem_criptografada.length() / colunas;
+    vector<pair<char,int>> colunas_ordenadas = geraPermutacao(chave);
+    vector<int> ordem_original(colunas);
+    for(int i = 0; i < colunas; i++) {
+        ordem_original[colunas_ordenadas[i].second - 1] = i;
+    }
+    string mensagem_decifrada;
+    for(int i = 0; i < linhas; i++) {
+        for(int &j : ordem_original) {
+            int pos = j  * linhas + i;
+            if(pos < mensagem_criptografada.size()) {
+                mensagem_decifrada += mensagem_criptografada[pos];
+            }
+        }
+    }
+    return mensagem_decifrada;
+}
+
+vector<pair<string,vector<int>>> ataqueForcaBruta(string mensagem_criptografada, int TAM_CHAVE) {
+    vector<int> permutacao(TAM_CHAVE);
+    vector<pair<string,vector<int>>> possibilidades;
+    for(int i = 0; i < TAM_CHAVE; i++) {
+        permutacao[i] = i + 1;
+    }
+    do {
+        int colunas = TAM_CHAVE;
+        int linhas = mensagem_criptografada.length() / colunas;
+        vector<int> ordem_original(colunas);
+        for(int i = 0; i < colunas; i++) {
+            ordem_original[permutacao[i] - 1] = i;
+        }
+        string mensagem_decifrada;
+        for(int i = 0; i < linhas; i++) {
+            for(int &j : ordem_original) {
+                int pos = j  * linhas + i;
+                if(pos < mensagem_criptografada.size()) {
+                    mensagem_decifrada += mensagem_criptografada[pos];
+                }
+            }
+        }
+        possibilidades.push_back({mensagem_decifrada, permutacao});
+    } while(next_permutation(begin(permutacao), end(permutacao)));
+    return possibilidades;
+}
+
+void geraResultadosForcaBruta(vector<pair<string,vector<int>>> possibilidades) {
+    for(auto &[mensagem, permutacao] : possibilidades) {
+        cout << '\n';
+        cout << "Resultado : " << mensagem << " \n\n | " << " Permutacao utilizada  : ";
+        for(int &i : permutacao) {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    } 
+}
---
+++ menu_cifra_transposicao.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+#include "cifra_transposicao.cpp"
+
+using namespace std;
+
+void LIMPA() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+void PAUSA() {
+    cout << "\nPressione Enter para continuar...";
+    cin.ignore(); cin.get();
+}
+string LER_ARQUIVO() {
+    ifstream arquivo("input2.txt");
+    string conteudo((istreambuf_iterator<char>(arquivo)), istreambuf_iterator<char>());
+    arquivo.close();
+    return conteudo;
+}
+void AVISO() {
+    LIMPA();
+    cout << "ATENCAO:\n";
+    cout << "O texto salvo no arquivo input.txt sera utilizado para as seguintes operacoes.\n";
+    cout << "Caso deseje alterar o plaintext, faca isso antes de prosseguir.";
+    PAUSA();
+}
+void CRIPTOGRAFAR() {
+    AVISO();
+    string original = LER_ARQUIVO();
+    string chave;
+    cout << "Digite a chave de transposicao (Recomendada: ""GAME""): ";
+    cin >> chave;
+    string cifrado = codificaTransposicao(original, chave);
+    cout << '\n';
+    cout << "\nMensagem criptografada:\n";
+    cout << cifrado << '\n';
+}
+void DECRIPTOGRAFAR() {
+    AVISO();
+    string mensagem = LER_ARQUIVO();
+    string chave;
+    cout << "Digite a chave utilizada: ";
+    cin >> chave;
+    string decifrado = decodificaTransposicao(mensagem, chave);
+    cout << '\n';
+    cout << "\nMensagem descriptografada:\n";
+    cout << decifrado << '\n';
+}
+void BRUTEFORCE() {
+    AVISO();
+    string mensagem = LER_ARQUIVO();
+    int tamanho;
+    cout << "Digite o tamanho fixo da chave de transposicao: ";
+    cin >> tamanho;
+    cout << "\nRealizando ataque brute-force...\n";
+    auto possibilidades = ataqueForcaBruta(mensagem, tamanho);
+    geraResultadosForcaBruta(possibilidades); 
+}
+void DISFREQ() {
+    AVISO();
+    string mensagem = LER_ARQUIVO();
+    int tamanho;
+    cout << "Digite o tamanho fixo da chave de transposicao: ";
+    cin >> tamanho;
+    vector<pair<string,pair<double,vector<int>>>> resultado = ataqueFrequencia(mensagem, tamanho);
+    cout << '\n';
+    cout << "\nMelhor score pela analise: " << resultado[0].second.first << '\n';
+    cout << "Mensagem decifrada:\n";
+    cout << resultado[0].first << '\n';
+    cout << "\nPermutacao utilizada : ";
+    for(int &i : resultado[0].second.second) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+}
+int main() {
+    ifstream teste("input2.txt");
+    if (!teste.good()) {
+        ofstream criar("input2.txt");
+        criar << "Texto padrao para testes. Substitua este conteudo.\n";
+        criar.close();
+    }
+    teste.close();
+    int OPCAO;
+    do {
+        LIMPA();
+        cout << "       SIMULADOR DE CIFRA POR TRANSPOSICAO \n\n";
+        cout << "1. Criptografar texto\n";
+        cout << "2. Descriptografar texto\n";
+        cout << "3. Ataque por brute-force\n";
+        cout << "4. Ataque por distribuicao de frequencias\n";
+        cout << "5. Sair\n";
+        cout << "\nEscolha uma opcao: ";
+        cin >> OPCAO;
+        LIMPA();
+        switch(OPCAO) {
+            case 1: CRIPTOGRAFAR(); break;
+            case 2: DECRIPTOGRAFAR(); break;
+            case 3: BRUTEFORCE(); break;
+            case 4: DISFREQ(); break;
+            case 5: cout << "Encerrando programa...\n"; return 0;
+            default: cout << "Opcao invalida!\n";
+        }
+        PAUSA();
+    } while(1);
+}
